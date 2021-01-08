65f16018aaad545067c4e97e6a83a449030f5d5e
Foster Horak <fosterhorak@macbook-pro.attlocal.net>
2021-01-08 14:00:47 -0600

    Complete step 3.3

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Widget
+from .forms import WidgetForm
 
 # Create your views here.
 def index(request):
     widgets = Widget.objects.all()
-    return render(request, 'index.html', {'widgets': widgets})
+    add_widget_form = WidgetForm()
+
+    return render(request, 'index.html', {
+        'widgets': widgets, 
+        'add_widget_form': add_widget_form
+    })
