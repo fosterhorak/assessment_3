fda7c529ad78ca96dc09b967a56b3fe5a07b02e2
Foster Horak <fosterhorak@macbook-pro.attlocal.net>
2021-01-08 15:38:27 -0600

    bonus attempted...

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Count, Avg
 from .models import Widget
 from .forms import WidgetForm
 
@@ -6,15 +7,20 @@ from .forms import WidgetForm
 def index(request):
     widgets = Widget.objects.all()
     add_widget_form = WidgetForm()
+    quantity_avg = Widget.objects.all().aggregate(Avg('quantity'))
+    quantity_count = Widget.objects.all().aggregate(Count('quantity'))
+    quantity_total = 10
 
     return render(request, 'index.html', {
         'widgets': widgets, 
-        'add_widget_form': add_widget_form
+        'add_widget_form': add_widget_form,
+        # 'quantity_total': quantity_total,
+        # 'quantity_avg': quantity_avg.quantity_avg,
+        # 'quantity_count': quantity_count.quantity_count,
     })
 
 def add_widget(request):
     form = WidgetForm(request.POST)
-    print('add_widget function is running')
 
     if form.is_valid():
         new_widget = form.save()
