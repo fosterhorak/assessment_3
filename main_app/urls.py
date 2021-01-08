ddd5a2808ba59c7a7868b1275cb0419778bfb44a
Foster Horak <fosterhorak@macbook-pro.attlocal.net>
2021-01-08 14:52:25 -0600

    Complete step 3.4

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
+    path('add_widget/', views.add_widget, name='add_widget'),
+    path('delete/<int:widget_id>', views.delete_widget, name='delete_widget'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Widget
 from .forms import WidgetForm
 
@@ -11,3 +11,21 @@ def index(request):
         'widgets': widgets, 
         'add_widget_form': add_widget_form
     })
+
+def add_widget(request):
+    form = WidgetForm(request.POST)
+    print('add_widget function is running')
+
+    if form.is_valid():
+        new_widget = form.save()
+    
+    # widgets = Widget.objects.all()
+    # return redirect('index', widgets = widgets)
+
+    return redirect('/')
+
+
+
+def delete_widget(request, widget_id):
+    Widget.objects.get(id=widget_id).delete()
+    return redirect('/')
